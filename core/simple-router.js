3487b60ce152898be42dc5f769ecf7b5c501529a
YinYong <yy15151877621@126.com>
2014-06-12 16:15:04 +0800

    [Update] give a demo

--- core/simple-router.js
+++ core/simple-router.js
@@ -1,7 +1,8 @@
 // exports
 var exp = module.exports = exports;
 var parseURL = require('url').parse,
-	fs = require('fs');
+	fs = require('fs'),
+	util = require('util');
 
 const METHOD_GET = 'get';
 const METHOD_POST = 'post';
@@ -9,6 +10,29 @@ const METHOD_UNKNOWN = 'unknown';
 
 const KEY_METHOD = 'method';
 const KEY_ROUTE = 'route';
+
+/*
+	// demo
+	var router = require('node-simple-router').newSimpleRouter();
+	router
+		.get('/login', function(req, res, data){
+				
+			})
+		.post('/check', function(req, res, data){
+				router.redirect('/order', req, res); // --> redirect route
+			})
+		.post('/order', function(req, res, data){
+				
+			})
+		.push('put', '/file', function(req, res, data){
+		
+			})
+		;
+		
+	http.createServer(router).listen(80).on('error', function(e){
+		console.log('err', e);
+	})
+*/
 
 // public function
 exp.createRouter = function() {
@@ -33,40 +57,42 @@ function configRouter() {
 		
 	self.methods = methods;
 	// for chained invoking
-	self.get = function (path, callback) { /* callback(req, res, method, route) */
+	self.get = function (path, callback) { /* callback(req, res, extras) */
 		get_cbs[path] = callback;
 		return self;
 	};
 	
-	self.post = function (path, callback) { /* callback(req, res, method, route) */
+	self.post = function (path, callback) { /* callback(req, res, extras) */
 		post_cbs[path] = callback;
 		return self;
 	};
 	
-	self.unKnownMethod = function (callback) { /* callback(req, res, method, route) */
+	self.unKnownMethod = function (callback) { /* callback(req, res, extras) */
 		unknowns[KEY_METHOD] = callback;
 		return self;
 	};
 	
 	// push a new configure with target method and route
-	self.push = function (method, route, callback){ /* callback(req, res, method, route) */
+	self.push = function (method, route, callback){ /* callback(req, res, extras) */
 		method = method.toLowerCase();
 		var target_method_cbs = methods[method] = methods[method] || {};
 		target_method_cbs[route] = callback;
 		return self;
 	};
 	
-	self.unKnownRoute = function (callback) { /* callback(req, res, method, route) */
+	self.unKnownRoute = function (callback) { /* callback(req, res, extras) */
 		unknowns[KEY_ROUTE] = callback;
 		return self;
 	};
 	
-	self.redirect = function (method, route, req, res) {
-		method = method.toLowerCase();
+	self.redirect = function (route, req, res) {
+		var extras = generateExtraData(req, res);
+		var method = extras.method;
 		var target_method_routes = methods[method] || {};
 		var target_method_cbs = target_method_routes[route];
 		if (target_method_cbs) {
-			target_method_cbs(req, res, method, route);
+			extras.pathname = route;
+			target_method_cbs(req, res, extras);
 		}
 		return self;
 	};
@@ -85,25 +111,54 @@ function configRouter() {
 function parse(req, res) {
 	var self = this;
 	
-	var method = req.method.toLowerCase();
-	console.log('router req method = %s, url = %s ', method, req.url);
+	var extras = generateExtraData(req, res);
+	console.log('router req extras = %s', util.inspect(extras));
 	
-	var tar_method = self.methods[method];
-	
-	var urlData = parseURL(req.url, true, true);
-	var pathname = urlData.pathname;
-	var querystring = urlData.query;
-		
+	var tar_method = self.methods[extras.method];
+	var tar_route = extras.pathname;
 	if (tar_method) {
-		var cb_ofpath = tar_method[pathname];
+		var cb_ofpath = tar_method[tar_route];
 		if (cb_ofpath) {
-			cb_ofpath(req, res, method, pathname, querystring);
+			cb_ofpath(req, res, extras);
 		} else {
 			// no matched paths
-			self.__dispatchUnknown(KEY_ROUTE, req, res, method, pathname, querystring);
+			self.__dispatchUnknown(KEY_ROUTE, req, res, extras);
 		}
 	} else {
 		// no matched methods
-		self.__dispatchUnknown(KEY_METHOD, req, res, method, pathname, querystring);
+		self.__dispatchUnknown(KEY_METHOD, req, res, extras);
 	}
+}
+
+/*
+req
+	headers: { 
+		host: 'localhost', --> 请求中的host
+		connection: 'keep-alive',
+		accept: '*\/*',
+		'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36',
+		'accept-encoding': 'gzip,deflate,sdch',
+		'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6' 
+	},
+	url: '/favicon.ico',
+	method: 'GET',
+*/
+function generateExtraData(req, res) {
+	var extras = {};
+	
+	// extras::headers
+	extras.headers = req.headers || {};
+	// extras::host
+	extras.host = extras.headers.host;
+	// extras::url
+	extras.url = req.url;
+	// extras::method
+	var method = req.method || '';
+	extras.method = method.toLowerCase();
+	
+	var urlData = parseURL(req.url, true, true);
+	urlData.pathname ? extras.pathname = urlData.pathname : {};
+	urlData.query ? extras.query = urlData.query : {};
+	urlData.hash ? extras.hash = urlData.hash : {};
+	return extras;
 }
